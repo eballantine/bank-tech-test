9dc20dc0bac3569730c0a19b1eef6d018f0cc1d1
eballantine <lizzy.kayballantine@gmail.com>
2021-09-27 14:56:06 +0100

    Refactor to separate print statement and calculate balance responsibilities

--- lib/bankaccount.rb
+++ lib/bankaccount.rb
@@ -17,11 +17,19 @@ class BankAccount
 
   def print_statement
     statement = "date || credit || debit || balance"
-    balance = 0
-    @transactions.each do |transaction|
-      balance += transaction[:amount]
-      statement += "\n #{transaction[:amount]}.00 || #{balance}.00"
+    @transactions.each_with_index do |transaction, i|
+      statement += "\n #{transaction[:amount]}.00 || #{calc_balance(i)}.00"
     end
     statement
+  end
+
+  private
+
+  def calc_balance(i)
+    balance = 0
+    @transactions[0..i].each do |transaction|
+      balance += transaction[:amount]
+    end
+    balance
   end
 end
