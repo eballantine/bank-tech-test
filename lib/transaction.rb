e5c9dc760516489f922896e9f58f3859f689bd08
eballantine <lizzy.kayballantine@gmail.com>
2021-09-29 13:26:55 +0100

    Add unit test to transaction spec

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,13 +5,13 @@ require 'Date'
 # This class is responsible for creating account transactions (deposits and withdrawals)
 class Transaction
   def create(type, amount)
-    # { type: type, date: transaction_date, amount: amount.round(2) }
+    { type: type, date: transaction_date, amount: amount.round(2) }
   end
 
-  # private
+  private
 
-  # def transaction_date
-  #   date = Date.today.to_s
-  #   Date.parse(date).strftime('%d/%m/%Y').to_s
-  # end
+  def transaction_date
+    date = Date.today.to_s
+    Date.parse(date).strftime('%d/%m/%Y').to_s
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,4 +4,11 @@ require 'transaction'
 
 describe Transaction do
   it { should respond_to(:create).with(2).arguments }
+
+  describe '.create' do
+    it 'should create a hash with 3 key/value pairs, type date and amount' do
+      allow(Date).to receive(:today).and_return('2021-01-01')
+      expect(subject.create(:deposit, 10.00)).to eq({ type: :deposit, date: '01/01/2021', amount: 10.00 })
+    end
+  end
 end
