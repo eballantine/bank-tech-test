6a0cd780b2a28c4e17af41578eb5d247fc4178e9
eballantine <lizzy.kayballantine@gmail.com>
2021-09-29 13:28:14 +0100

    Add feature tests. Uncomment everything

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,38 +2,38 @@
 
 # This class is responsible for generating bank statements
 class Statement
-  # def initialize(transactions)
-  #   @transactions = transactions
-  #   @statement = +''
-  # end
+  def initialize(transactions)
+    @transactions = transactions
+    @statement = +''
+  end
 
-  # def print_statement
-  #   assemble_statement_entries if @transactions.length > 0
-  #   @statement.prepend('date || credit || debit || balance')
-  #   puts @statement
-  # end
+  def print_statement
+    assemble_statement_entries if @transactions.length > 0
+    @statement.prepend('date || credit || debit || balance')
+    puts @statement
+  end
 
-  # private
+  private
 
-  # def create_statement_entry(transaction, index)
-  #   if transaction[:type] == :deposit
-  #     "\n#{transaction[:date]} || #{format('%.2f', transaction[:amount])} || || #{calc_running_balance(index)}"
-  #   else
-  #     "\n#{transaction[:date]} || || #{format('%.2f', transaction[:amount])} || #{calc_running_balance(index)}"
-  #   end
-  # end
+  def create_statement_entry(transaction, index)
+    if transaction[:type] == :deposit
+      "\n#{transaction[:date]} || #{format('%.2f', transaction[:amount])} || || #{calc_running_balance(index)}"
+    else
+      "\n#{transaction[:date]} || || #{format('%.2f', transaction[:amount])} || #{calc_running_balance(index)}"
+    end
+  end
 
-  # def calc_running_balance(index)
-  #   balance = 0
-  #   @transactions[0..index].each do |transaction|
-  #     transaction[:type] == :deposit ? balance += transaction[:amount] : balance -= transaction[:amount]
-  #   end
-  #   format('%.2f', balance)
-  # end
+  def calc_running_balance(index)
+    balance = 0
+    @transactions[0..index].each do |transaction|
+      transaction[:type] == :deposit ? balance += transaction[:amount] : balance -= transaction[:amount]
+    end
+    format('%.2f', balance)
+  end
 
-  # def assemble_statement_entries
-  #   @transactions.each_with_index do |transaction, index|
-  #     @statement.prepend(create_statement_entry(transaction, index))
-  #   end
-  # end
+  def assemble_statement_entries
+    @transactions.each_with_index do |transaction, index|
+      @statement.prepend(create_statement_entry(transaction, index))
+    end
+  end
 end
