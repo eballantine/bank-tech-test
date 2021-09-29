cdb22e4876fb12f7b21266831c4efbe3364e7c0e
eballantine <lizzy.kayballantine@gmail.com>
2021-09-29 15:33:32 +0100

    Move statement unit tests to statement spec

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,16 +3,42 @@
 require 'statement'
 
 describe Statement do
-  let(:statement) { described_class.new([]) }
+  let(:statement_no_transactions) { described_class.new([]) }
+  let(:statement_with_transactions) { described_class.new([
+    { type: :deposit, date: '01/01/2021', amount: 100.00 },
+    { type: :withdrawal, date: '03/10/2022', amount: 10.00 }
+  ]) }
 
   it 'should respond to print_statment with no arguments' do
-    expect(statement).to respond_to(:print_statement).with(0).arguments
+    expect(statement_no_transactions).to respond_to(:print_statement).with(0).arguments
   end
 
   describe '.print_statement' do
-    it 'should puts statement to terminal' do
-      expect { statement.print_statement }.to output(
-        "date || credit || debit || balance\n").to_stdout
+    it 'should print column headers' do
+      expect { statement_no_transactions.print_statement }.to output("date || credit || debit || balance\n").to_stdout
+    end
+
+    it 'should print the transaction date' do
+      expect { statement_with_transactions.print_statement }.to output(include('01/01/2021')).to_stdout
+    end
+
+    it 'should print deposits in the second column with empty third column' do
+      expect { statement_with_transactions.print_statement }.to output(include('|| 100.00 || ||')).to_stdout
+    end
+
+    it 'should print withdrawals in the third column with empty second column' do
+      expect { statement_with_transactions.print_statement }.to output(include('|| || 10.00 ||')).to_stdout
+    end
+
+    it 'should print account balance after each transaction has completed' do
+      expect { statement_with_transactions.print_statement }.to output(include('90.00')).to_stdout
+    end
+
+    it 'should print transactions in reverse chronological order' do
+      expect { statement_with_transactions.print_statement }.to output(
+        "date || credit || debit || balance\n"\
+        "03/10/2022 || || 10.00 || 90.00\n01/01/2021 || 100.00 || || 100.00\n"
+      ).to_stdout
     end
   end
 end
