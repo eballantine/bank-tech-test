baedab6d31353dc91af67a0b5200766a44f21970
eballantine <lizzy.kayballantine@gmail.com>
2021-09-29 13:23:24 +0100

    Add unit test for Statement's print_statement method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,44 +5,44 @@ require_relative 'statement'
 
 # This class is responsible for allowing a user to interact with their bank account
 class BankAccount
-  attr_reader :transactions
+  # attr_reader :transactions
 
-  def initialize
-    @transactions = []
-  end
+  # def initialize
+  #   @transactions = []
+  # end
 
-  def deposit(amount)
-    check_validity(amount)
-    @transactions << Transaction.new.create(:deposit, amount)
-    'Deposit complete'
-  end
+  # def deposit(amount)
+  #   check_validity(amount)
+  #   @transactions << Transaction.new.create(:deposit, amount)
+  #   'Deposit complete'
+  # end
 
-  def withdraw(amount)
-    check_validity(amount)
-    return 'Insufficient funds to make this withdrawal' if amount > calc_balance
+  # def withdraw(amount)
+  #   check_validity(amount)
+  #   return 'Insufficient funds to make this withdrawal' if amount > calc_balance
 
-    @transactions << Transaction.new.create(:withdrawal, amount)
-    'Withdrawal complete'
-  end
+  #   @transactions << Transaction.new.create(:withdrawal, amount)
+  #   'Withdrawal complete'
+  # end
 
-  def print_statement
-    @statement = Statement.new(@transactions)
-    @statement.print_statement
-  end
+  # def print_statement
+  #   @statement = Statement.new(@transactions)
+  #   @statement.print_statement
+  # end
 
-  private
+  # private
 
-  def calc_balance
-    balance = 0
-    @transactions.each do |transaction|
-      transaction[:type] == :deposit ? balance += transaction[:amount] : balance -= transaction[:amount]
-    end
-    balance
-  end
+  # def calc_balance
+  #   balance = 0
+  #   @transactions.each do |transaction|
+  #     transaction[:type] == :deposit ? balance += transaction[:amount] : balance -= transaction[:amount]
+  #   end
+  #   balance
+  # end
 
-  def check_validity(amount)
-    raise 'Please provide the amount in pounds and pence, e.g. 10.00' unless amount.is_a? Float
-    raise 'Deposit amount must be positive' if amount <= 0
-    raise 'Please provide the amount in pounds and pence, e.g. 10.00' if amount.round(2) != amount
-  end
+  # def check_validity(amount)
+  #   raise 'Please provide the amount in pounds and pence, e.g. 10.00' unless amount.is_a? Float
+  #   raise 'Deposit amount must be positive' if amount <= 0
+  #   raise 'Please provide the amount in pounds and pence, e.g. 10.00' if amount.round(2) != amount
+  # end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,4 +8,11 @@ describe Statement do
   it 'should respond to print_statment with no arguments' do
     expect(statement).to respond_to(:print_statement).with(0).arguments
   end
+
+  describe '.print_statement' do
+    it 'should puts statement to terminal' do
+      expect { statement.print_statement }.to output(
+        "date || credit || debit || balance\n").to_stdout
+    end
+  end
 end
